b9783de371d02ae532ba9b156f061d4f59892ee6
Markus Maal <markus.maal@gmail.com>
2025-05-20 15:13:39 +0300

    Improved protections against MySQL injections

--- sync.php
+++ sync.php
@@ -18,16 +18,26 @@ foreach (json_decode($remote_data) as $record) {
     $arr = mysqli_fetch_array(mysqli_query($connection, "SELECT COUNT(*), ID FROM channel_db WHERE URL = '$url'"));
     $result = $arr[0];
     $id = $arr[1];
+    //
+    // *** ANTI-HACKING STUFF - DO NOT TRUST ANY INPUT! ***
+    //
+    // This is really hard to hack anyway, because the request is made
+    // from local server, but we just want to be really safe here.
+    //
+    // Simple int check works for booleans, but if it's a string
+    // a more sophisticated sanitization with mysqli_real_escape_string
+    // is required.
+    //
     $values = array(
         tstr($record->Kanal, $connection),
         tstr($record->Video, $connection),
-        $record->Kustutatud,
+        is_int($record->Kustutatud) ? $record->Kustutatud : 0,
         tstr($record->Kuupäev, $connection),
         tstr($record->Kirjeldus, $connection),
-        $record->Subtiitrid,
-        $record->Avalik,
-        $record->Ülekanne,
-        $record->HD,
+        is_int($record->Subtiitrid) ? $record->Subtiitrid : 0,
+        is_int($record->Avalik) ? $record->Avalik : 0,
+        is_int($record->Ülekanne) ? $record->Ülekanne : 0,
+        is_int($record->HD) ? $record->HD : 0,
         tstr($record->URL, $connection),
         tstr($record->TitleMUI_en, $connection),
         tstr($record->TitleMUI_et, $connection),
@@ -40,7 +50,7 @@ foreach (json_decode($remote_data) as $record) {
         tstr($record->OdyseeURL, $connection),
         tstr($record->KanalMUI_en, $connection),
         tstr($record->KanalMUI_et, $connection),
-        1
+        1 // this is fine, because constant
     );
     if ($result == 0) {
         $query = "INSERT INTO channel_db (Kanal, Video, Kustutatud, Kuupäev, Kirjeldus, Subtiitrid, Avalik, Ülekanne, HD, URL, TitleMUI_en, TitleMUI_et, KirjeldusMUI_en, KirjeldusMUI_et, Filename, Category, CategoryMUI_en, Tags, OdyseeURL, KanalMUI_en, KanalMUI_et, Translated) VALUES (" . join(", ", $values) . ")";
@@ -54,7 +64,7 @@ foreach (json_decode($remote_data) as $record) {
                 $setstr .= ", ";
             }
         }
-        $query = "UPDATE channel_db " . $setstr . " WHERE ID = " . $id;
+        $query = "UPDATE channel_db " . $setstr . " WHERE ID = " . $id; // this is fine, because ID is derived from local database
         mysqli_query($connection, $query);
     }
 }
